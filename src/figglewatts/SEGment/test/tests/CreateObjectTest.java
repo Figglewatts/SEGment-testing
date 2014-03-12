c0bdc2b0e0c37335b0dc2121688dd3d46ef44333
Sam Gibson <figglewatts@hotmail.co.uk>
2014-03-12 10:22:03 +0000

    Added basis of first test

---
+++ src/figglewatts/SEGment/test/tests/CreateObjectTest.java
@@ -0,0 +1,13 @@
+package figglewatts.SEGment.test.tests;
+
+import figglewatts.SEGment.test.BaseTest;
+
+public class CreateObjectTest extends BaseTest {
+
+	@Override
+	public void Execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
