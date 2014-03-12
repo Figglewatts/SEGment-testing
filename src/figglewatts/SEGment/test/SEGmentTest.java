7ef9876c4e9a0b1d00e2f66bd7885f0911018fee
Sam Gibson <figglewatts@hotmail.co.uk>
2014-03-12 10:00:04 +0000

    Removed fluff in SEGmentTest.java

--- src/figglewatts/SEGment/test/SEGmentTest.java
+++ src/figglewatts/SEGment/test/SEGmentTest.java
@@ -1,25 +1,8 @@
 package figglewatts.SEGment.test;
 
-import figglewatts.SEGment.ComponentEngine;
-
 public class SEGmentTest {
 
 	public static void main(String[] args) throws Exception {
-		System.out.println("Running...");
-		TestObject object = new TestObject();
-		RandomComponent randomComponent = new RandomComponent();
-		randomComponent.setMultiplier(5);
-		RandomerComponent randomerComponent = new RandomerComponent();
-		randomerComponent.setNumber(8);
-		object.components.add(randomComponent);
-		object.components.add(randomerComponent);
-		MultiplierNode node = new MultiplierNode();
 		
-		MultiplierSystem system = new MultiplierSystem();
-		ComponentEngine.registerNode(node);
-		ComponentEngine.registerObject(object);
-		ComponentEngine.addSystem(system);
-
-		System.out.println(((RandomerComponent) object.components.get("RandomerComponent")).getNumber());
 	}
 }
